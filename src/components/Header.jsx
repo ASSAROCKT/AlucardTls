a515bb571449fbad4832fd32bbe53042970c6fb9
Sriram <a.sriram.ramesh@gmail.com>
2025-08-06 18:40:46 +0530

    Changed url

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,7 +18,7 @@ function Header() {
             <a href="https://discord.gg/x22HkcVKHT" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
               <i className="fab fa-discord text-2xl"></i>
             </a>
-            <a href="https://ko-fi.com/aphroditescans" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
+            <a href="https://ko-fi.com/alucardnovels" target="_blank" rel="noopener noreferrer" className="text-gray-300 hover:text-white transition duration-300 ease-in-out">
               <i className="fas fa-mug-hot text-2xl"></i>
             </a>
           </div>
